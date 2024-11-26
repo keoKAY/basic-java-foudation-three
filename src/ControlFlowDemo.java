f62096173723e62a70714619297551cc6af8d9e2
Keo Kay <keokay888@gmail.com>
2024-11-26 15:21:33 +0700

    latest commit

---
+++ src/ControlFlowDemo.java
@@ -0,0 +1,22 @@
+// Ternary vs if else
+public class ControlFlowDemo {
+    public static void main(String[] args) {
+        // if - else
+        // if else if else
+        // ternary operator -> ? :
+
+        char gender = 'm'; // 'm'
+        if (gender == 'f') {
+            System.out.println("Gender is : female");
+        } else {
+            System.out.println("Gender is : male ");
+        }
+        String renderedGender = (gender == 'f') ? "female" : "male";
+//        System.out.println("Gender is:"+((gender=='f')?"female":"male"));
+        System.out.println("Gender is : " + renderedGender);
+
+
+        // switch -> case
+
+    }
+}
---
+++ src/SwitchCaseDemo.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class SwitchCaseDemo {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int option;
+
+        do {
+            System.out.println("_______<< ISTAD Restarant >>>______");
+            System.out.println("1. Fried Rice ");
+            System.out.println("2. Chicken Rice ");
+            System.out.println("3. Soup SiChoun ");
+            System.out.println("4. Water");
+            System.out.println("5. Exit ");
+
+            System.out.println("Choose option (1-4): ");
+            option = input.nextInt();
+            switch (option) {
+                case 1: {
+                    int price = 4000;
+                    System.out.println("Bay chaa ");
+                    System.out.println("Value of price : " + price);
+                    break;
+                }
+                case 2:
+                    int price=5000;
+                    System.out.println("Chicken rice ! ");
+                    System.out.println("Price is : "+price);
+                    break;
+                case 3:
+                    System.out.println("Soup SiChoun ");
+                    break;
+                case 4:
+                    System.out.println("Water");
+                    break;
+                case 5:
+                    System.out.println("Exit ");
+                    break;
+                default:
+                    System.out.println(" Invalid option!!! ");
+                    break;
+
+            }
+        } while (option != 5);
+    }
+}
